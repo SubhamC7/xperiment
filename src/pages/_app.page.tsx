2f8c212306491fc829d1918d61e793361eb3f44c
SubhamC7 <subhamchatterjee2020@gmail.com>
2022-09-20 18:31:50 +0530

    test

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -6,6 +6,20 @@ import { useRouter } from "next/router";
 const publicUrls = ["/login", "/admin-login"];
 
 function MyApp({ Component, pageProps }: AppProps) {
+  //@ts-ignore
+  // if (window.netlifyIdentity) {
+  //   //@ts-ignore
+  //   window.netlifyIdentity.on("init", (user) => {
+  //     if (!user) {
+  //       //@ts-ignore
+  //       window.netlifyIdentity.on("login", () => {
+  //         document.location.href = "/admin/";
+  //       });
+  //     } else {
+  //       return <Component {...pageProps} />;
+  //     }
+  //   });
+  // }
   return <Component {...pageProps} />;
 }
 
