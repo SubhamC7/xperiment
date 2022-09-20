8cb7a4b891598ed20f361019a4921ee263c0687f
SubhamC7 <subhamchatterjee2020@gmail.com>
2022-09-21 01:47:41 +0530

    test

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,55 +1,87 @@
-import React from "react";
+// import React from "react";
+
+// import Head from "next/head";
+// import { Component } from "react";
+// import { attributes, react as HomeContent } from "../content/home.md";
+
+// import AppStore from "AppStore";
+// type Props = {};
+
+// const Home = (props: Props) => {
+//   const [paddingClass] = AppStore("paddingClass");
+
+//   let { title, cats } = attributes;
+//   console.log("cats", cats);
+//   return (
+//     <>
+//       <Head>
+//         <title>Xperiment</title>
+//         <script
+//           src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+//           async
+//         ></script>
+//       </Head>
+//       <header
+//         className={`fixed top-0 right-0 h-20 w-full bg-rose-700 text-white flex items-center justify-between ${paddingClass}`}
+//       >
+//         <p className="text-white text-2xl font-semibold">Xperiment</p>
+//         <div className="flex items-center justify-center space-x-7">
+//           <p className="text-white font-semibold">Home</p>
+//           <p className="text-white font-semibold">About</p>
+//         </div>
+//       </header>
+//       <article className={`pt-24 ${paddingClass}`}>
+//         <h1 className="text-4xl font-semibold text-rose-600">{title}</h1>
+//         <HomeContent />
+//         <ul>
+//           {cats.map((cat: any, k: any) => (
+//             <div
+//               key={k}
+//               className="py-3 flex items-start justify-start space-x-4"
+//             >
+//               <div className="rounded-full h-4 w-4 bg-yellow-600 mt-2"></div>
+//               <div>
+//                 <h2 className="text-lg font-medium">{cat.name}</h2>
+//                 <p className="">-{cat.description}</p>
+//               </div>
+//             </div>
+//           ))}
+//         </ul>
+//       </article>
+//     </>
+//   );
+// };
+
+// export default Home;
 
 import Head from "next/head";
 import { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
 
-import AppStore from "AppStore";
-type Props = {};
-
-const Home = (props: Props) => {
-  const [paddingClass] = AppStore("paddingClass");
-
-  let { title, cats } = attributes;
-  console.log("cats", cats);
-  return (
-    <>
-      <Head>
-        <title>Xperiment</title>
-        <script
-          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          async
-        ></script>
-      </Head>
-      <header
-        className={`fixed top-0 right-0 h-20 w-full bg-rose-700 text-white flex items-center justify-between ${paddingClass}`}
-      >
-        <p className="text-white text-2xl font-semibold">Xperiment</p>
-        <div className="flex items-center justify-center space-x-7">
-          <p className="text-white font-semibold">Home</p>
-          <p className="text-white font-semibold">About</p>
-        </div>
-      </header>
-      <article className={`pt-24 ${paddingClass}`}>
-        <h1 className="text-4xl font-semibold text-rose-600">{title}</h1>
-        <HomeContent />
-        <ul>
-          {cats.map((cat: any, k: any) => (
-            <div
-              key={k}
-              className="py-3 flex items-start justify-start space-x-4"
-            >
-              <div className="rounded-full h-4 w-4 bg-yellow-600 mt-2"></div>
-              <div>
-                <h2 className="text-lg font-medium">{cat.name}</h2>
-                <p className="">-{cat.description}</p>
-              </div>
-            </div>
-          ))}
-        </ul>
-      </article>
-    </>
-  );
-};
-
-export default Home;
+export default class Home extends Component {
+  render() {
+    let { title, cats } = attributes;
+    return (
+      <>
+        <Head>
+          <script
+            src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+            async
+          ></script>
+        </Head>
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat: any, k: any) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </>
+    );
+  }
+}
