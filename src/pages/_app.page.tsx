a5bf3b947212e28b343d7efca57579a90aa9e662
SubhamC7 <subhamchatterjee2020@gmail.com>
2022-09-20 17:46:44 +0530

    fix app_page

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,50 +2,10 @@ import AppStore from "AppStore";
 import "assets/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import Admin from "./admin/index.page";
-import Home from "./home/index.page";
-import Login from "./login/index.page";
-import AdminLogin from "./admin-login/index.page";
 
 const publicUrls = ["/login", "/admin-login"];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [user] = AppStore("user");
-  const [referralUser] = AppStore("referralUser");
-  const hasUser = Object.keys(user).length;
-  const hasReferralUser = Object.keys(referralUser).length;
-  const isPublic = publicUrls.indexOf(router.pathname) > -1;
-
-  // if (router.pathname.includes("/home")) {
-  //   if (hasReferralUser && isPublic) {
-  //     return <Home />;
-  //   } else if (hasReferralUser && !isPublic) {
-  //     return <Login />;
-  //   }
-  // } else if (router.pathname.includes("/admin")) {
-  //   if (hasUser && isPublic) {
-  //     return <Admin />;
-  //   } else {
-  //     return <AdminLogin />;
-  //   }
-  // }
-  // else {
-  //   return <Component {...pageProps} />;
-  // }
-
-  // if (hasReferralUser && isPublic && !hasUser) {
-  //   return <Home />;
-  // } else if (!hasUser && !isPublic && !hasReferralUser) {
-  //   return <AdminLogin />;
-  // } else if (hasUser && isPublic && !hasReferralUser) {
-  //   return <Admin />;
-  // } else if (!hasReferralUser && !isPublic && !hasUser) {
-  //   return <Login />;
-  // } else {
-  //   return <Component {...pageProps} />;
-  // }
-
   return <Component {...pageProps} />;
 }
 
